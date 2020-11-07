d0eb8f5b70dc80b80b266a02d4c890eb98291982
Andrew Moses <andrewmosesdrive@gmail.com>
2020-11-06 18:45:09 -0700

    added initial filepaths and started connection; created db schema and seeds; installed npm packages

---
+++ db/config/orm.js
@@ -0,0 +1,14 @@
+// Import MySQL connection.
+const connection = require("../config/connection.js");
+
+const orm = {
+
+selectBurger:
+
+insertBurger:
+
+updateBurger:
+
+}
+// Export the orm object for the model (cat.js).
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Quarter Pounder", FALSE), ("Royale with Cheese", FALSE), ("Whopper", FALSE);
