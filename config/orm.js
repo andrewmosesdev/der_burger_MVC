f063e46db4477fa05e750799fb49926688aec63b
Andrew Moses <andrewmosesdrive@gmail.com>
2020-11-06 19:34:57 -0700

    initial orm methods created

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,38 @@
 // Import MySQL connection.
 const connection = require("./connection.js");
 
-// const orm = {
+const orm = {
 
-// selectBurger:
+// selectBurger
+// what's going to be passed into this function
+selectAllBurgers: function(burgerName, isDevoured) {
+    const selectQueryString = "SELECT * FROM ? WHERE ? = ?";
+    // variables to be passed as values into the table
+    connection.query(selectQueryString, [burgerName, isDevoured], function(err, selectedResults) {
+        if (err) throw err;
+        console.log(selectedResults);
+    })
+},
 
 // insertBurger:
+createBurger: function(burgerName, isDevoured) {
+    const createQueryString = "INSERT INTO burgers (burger_name, devoured)VALUES (?, ?)";
+    connection.query(createQueryString, [burgerName, isDevoured], function(err, createdResults) {
+        if (err) throw err;
+        console.log(createdResults);
+    })
+},
 
 // updateBurger:
+updateBurger: function(burgerName, isDevoured) {
+    const updateQueryString = "";
+    connection.query(updateQueryString, [burgerName, isDevoured], function(err, updateResults) {
+        if (err) throw err;
+        console.log(updateResults)
+    })
+}
+
+}
 
 // }
 // Export the orm object for the model (cat.js).
