72d448b42ec794e7813929d9d857d52d3ac49892
Andrew Moses <andrewmosesdrive@gmail.com>
2020-11-08 16:20:34 -0700

    updated burger.js to call orm methods

--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,23 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-    // pull orm method to select all burgers
-    all: function(cb) {
-        orm.all("burgers", function(res) {
-            cb(res)
-        })
-    },
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, devoured, cb) {
+    orm.update("burgers", objColVals, devoured, function(res) {
+      cb(res);
+    });
+  },
 
-    // pull orm method to create new burgers
-    create: function(cb) {
-        orm.create("burgers", function(res) {
-            cb(res)
-        })
-    },
-
-    // orm method to update existing burger
-    // need to add in a way to read t/f for boolean
-    update: function(cb) {
-        orm.update("burgers", function(res) {
-            cb(res)
-        })
-    }
-}
+};
 
 module.exports = burger;
