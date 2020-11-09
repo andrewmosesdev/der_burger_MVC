a001ce2cc2afe14a7668f6c2fe8f241f7249fb5d
Andrew Moses <andrewmosesdrive@gmail.com>
2020-11-08 17:22:34 -0700

    updated burgers.js to include ajax calls for onclick events

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,10 +2,16 @@
 $(function() {
   $(".change-devoured").on("click", function(event) {
     const id = $(this).data("id");
+    // const newDevoured = $(this).data("true")
+
+    // const newDevouredState = {
+    //   devoured: newDevoured
+    // }
 
     // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
+      // data: newDevouredState
     }).then(
       function() {
         // Reload the page to get the updated list
