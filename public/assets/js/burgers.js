1655165b7e7ce9b23d26520ee89bd02397626246
Andrew Moses <andrewmosesdrive@gmail.com>
2020-11-08 17:01:14 -0700

    updated burgers.js for ajax calls utilizing jquery

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,42 +1,39 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-   
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      const newBurger = {
-        name: $("#brgr").val().trim(),
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+  $(".change-devoured").on("click", function(event) {
+    const id = $(this).data("id");
 
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+    }).then(
+      function() {
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 
-    $(".change-devoured").on("click", function(event) {
-        const id = $(this).data("id");
-    
-        // Send the PUT request.
-        $.ajax("/api/burgers/" + id, {
-          type: "PUT",
-        }).then(
-          function() {
-            console.log("devoured!");
-            // Reload the page to get the updated list
-            location.reload();
-          }
-        );
-      });
-  
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    const newBurger = {
+      burger_name: $("#burger-name").val().trim(),
+      devoured: $("[name=burger-devoured]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
 });
-  
